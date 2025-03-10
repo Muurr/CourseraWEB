1afbeeccd4aaf8dc72897d878140adf0acb0a956
Muurr <163830712+Muurr@users.noreply.github.com>
2025-03-11 00:54:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to log a message to the console
+function logMessage(message) {
+    console.log(message);
+}
+
+// Example usage of the logMessage function
+logMessage('Hello, World!');
